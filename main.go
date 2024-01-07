0c05797d59e0d9910f907e0019d252d74018231a
Евгений <112827365+get-route@users.noreply.github.com>
2024-01-08 01:27:52 +0300

    db install

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-	type User struct{
-		Name string
-		Age uint16
-		Money int16
-		Avg_grades, Happines float64
-	}
-//
+type User struct {
+	Name                 string `json:"name"`
+	Age                  uint16 `json:"age"`
+	Money                int16
+	Avg_grades, Happines float64
+}
+
 //	func (u User) getAllInfo () string{
 //		return fmt.Sprintf("User name is %s -- %d -- %d", u.name, u.age, u.money)
 //	}
@@ -35,8 +38,34 @@ func handleRequest() {
 	http.ListenAndServe(":4567", nil)
 }
 func main() {
-	handleRequest()
+	
 	//var bob User
 	//bob := User{name: "Bob", age: 25, money: -50, avg_grades: 33, happines: 0.5}
+	db, err :=sql.Open("mysql","slide:slide@tcp(127.0.0.1:3306)/golang")
+	if err !=nil {
+		panic(err)
+	}
+	defer db.Close()
+	//Установка базы
+	// insert, err :=db.Query("INSERT INTO `user` (`name`,`age`) VALUES ('Evg',34)")
+	// if err!=nil {
+	// 	panic(err)
+	// }
+	// defer insert.Close()
+	//handleRequest()
+	//Выборка 
+	res, err :=db.Query("SELECT `name`, `age` FROM `user`")
+	if err != nil {
+		
+	}
+	for res.Next(){
+		var user User
+		err = res.Scan(&user.Name, &user.Age)
+		if err != nil {
+		
+		}
+		fmt.Println(fmt.Sprintf("User: %s with age %d", user.Name, user.Age))
+	}
+	
 
 }
