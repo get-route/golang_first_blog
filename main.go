82f5962302430fe27ce9929fee58023d52f20a9c
Евгений <112827365+get-route@users.noreply.github.com>
2024-01-08 13:56:06 +0300

    form add articles

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http"
 	"html/template"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func index(w http.ResponseWriter, r *http.Request){
@@ -14,9 +17,38 @@ func index(w http.ResponseWriter, r *http.Request){
 	}
 	t.ExecuteTemplate(w, "index", nil)
 }
+func create(w http.ResponseWriter, r *http.Request){
+	t,err :=template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
+	if err!=nil {
+		fmt.Fprintf(w, err.Error())
+	
+	}
+	t.ExecuteTemplate(w, "create", nil)
+}
+func save_article(w http.ResponseWriter, r *http.Request){
+	  
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
+	db, err :=sql.Open("mysql","slide:slide@tcp(127.0.0.1:3306)/golang")
+	if err !=nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	insert, err :=db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`,`anons`, `full_text`) VALUES ('%s','%s','%s')", title, anons, full_text))
+	if err!=nil {
+		panic(err)
+	}
+	defer insert.Close()
+	http.Redirect(w,r,"/", http.StatusSeeOther)
+}
 func handleFunc(){
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))	
 	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe(":7878", nil)
 }
 
