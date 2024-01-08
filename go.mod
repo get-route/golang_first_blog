d6644cc15830828c255ddc0d870c48743357d0b4
Евгений <112827365+get-route@users.noreply.github.com>
2024-01-09 00:33:50 +0300

    db form and dinamic pages

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module my_site
 go 1.21.5
 
 require github.com/go-sql-driver/mysql v1.7.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"github.com/gorilla/mux"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+type Article struct{
+	Id uint16
+	Title, Anons, FullText string
+}
+var posts = []Article{}
+var showPost = Article{}
 
 func index(w http.ResponseWriter, r *http.Request){
 	t,err :=template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -15,7 +22,29 @@ func index(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, err.Error())
 	
 	}
-	t.ExecuteTemplate(w, "index", nil)
+	db, err :=sql.Open("mysql","slide:slide@tcp(127.0.0.1:3306)/golang")
+	if err !=nil {
+		panic(err)
+	}
+	defer db.Close()
+	//Выборка 
+	res, err :=db.Query("SELECT * FROM `articles`")
+	if err != nil {
+		
+	}
+	posts = []Article{}
+	for res.Next(){
+		var articles Article
+		err = res.Scan(&articles.Id, &articles.Title,&articles.Anons,&articles.FullText)
+		if err != nil {
+	
+			
+		}
+		
+		posts = append(posts, articles)
+		//fmt.Println(fmt.Sprintf("User: %s with age %d", articles.Title, articles.Id))
+	}
+	t.ExecuteTemplate(w, "index", posts)
 }
 func create(w http.ResponseWriter, r *http.Request){
 	t,err :=template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
@@ -30,8 +59,10 @@ func save_article(w http.ResponseWriter, r *http.Request){
 	title := r.FormValue("title")
 	anons := r.FormValue("anons")
 	full_text := r.FormValue("full_text")
-
-	db, err :=sql.Open("mysql","slide:slide@tcp(127.0.0.1:3306)/golang")
+	if title =="" || anons =="" || full_text =="" {
+		fmt.Fprintf(w,"Данные заполнены некорректно")
+	}else{
+		db, err :=sql.Open("mysql","slide:slide@tcp(127.0.0.1:3306)/golang")
 	if err !=nil {
 		panic(err)
 	}
@@ -42,13 +73,48 @@ func save_article(w http.ResponseWriter, r *http.Request){
 		panic(err)
 	}
 	defer insert.Close()
-	http.Redirect(w,r,"/", http.StatusSeeOther)
+	http.Redirect(w,r,"/", http.StatusSeeOther)	
+	}
+
+}
+func show_post(w http.ResponseWriter, r *http.Request)  {
+	vars := mux.Vars(r)
+	t,err :=template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+	if err!=nil {
+		fmt.Fprintf(w, err.Error())
+	
+	}
+	db, err :=sql.Open("mysql","slide:slide@tcp(127.0.0.1:3306)/golang")
+	if err !=nil {
+		panic(err)
+	}
+	defer db.Close()
+	//Выборка 
+	res, err :=db.Query(fmt.Sprintf("SELECT * FROM `articles` WHERE `id` = '%s'", vars["id"]))
+	if err != nil {
+		
+	}
+	showPost = Article{}
+	for res.Next(){
+		var articles Article
+		err = res.Scan(&articles.Id, &articles.Title,&articles.Anons,&articles.FullText)
+		if err != nil {
+			panic(err)
+		}
+		showPost = articles
+		//fmt.Println(fmt.Sprintf("User: %s with age %d", articles.Title, articles.Id))
+	}
+	t.ExecuteTemplate(w, "show", showPost)
+
 }
 func handleFunc(){
+	rtr :=mux.NewRouter()
+	rtr.HandleFunc("/", index).Methods("GET")
+	rtr.HandleFunc("/create", create).Methods("GET")
+	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET")
+	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	http.Handle("/",rtr)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))	
-	http.HandleFunc("/", index)
-	http.HandleFunc("/create", create)
-	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe(":7878", nil)
 }
 
