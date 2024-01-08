a5554ed5fb16097ec5c289c077490e9626a64a98
Евгений <112827365+get-route@users.noreply.github.com>
2024-01-08 13:12:18 +0300

    start serve option && routes list

--- main.go
+++ main.go
@@ -1,71 +1,25 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"html/template"
 	"net/http"
-
-	_ "github.com/go-sql-driver/mysql"
+	"html/template"
 )
 
-type User struct {
-	Name                 string `json:"name"`
-	Age                  uint16 `json:"age"`
-	Money                int16
-	Avg_grades, Happines float64
+func index(w http.ResponseWriter, r *http.Request){
+	t,err :=template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err!=nil {
+		fmt.Fprintf(w, err.Error())
+	
+	}
+	t.ExecuteTemplate(w, "index", nil)
+}
+func handleFunc(){
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))	
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":7878", nil)
 }
 
-//	func (u User) getAllInfo () string{
-//		return fmt.Sprintf("User name is %s -- %d -- %d", u.name, u.age, u.money)
-//	}
-//
-//	func(u *User) setNewName(newName string){
-//		u.name = newName
-//	}
-func home_page(page http.ResponseWriter, r *http.Request) {
-	bob := User{"BOb", 25, -50, 33.3, 0.5}
-	//fmt.Fprintf(page, "Имя юзера ")
-	tmpl, _ := template.ParseFiles("templates/home_page.html")
-	tmpl.Execute(page, bob)
-}
-func contacts_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Страница контактов это...")
-}
-func handleRequest() {
-	http.HandleFunc("/", home_page)
-	http.HandleFunc("/contacts/", contacts_page)
-	http.ListenAndServe(":4567", nil)
-}
-func main() {
-	
-	//var bob User
-	//bob := User{name: "Bob", age: 25, money: -50, avg_grades: 33, happines: 0.5}
-	db, err :=sql.Open("mysql","slide:slide@tcp(127.0.0.1:3306)/golang")
-	if err !=nil {
-		panic(err)
-	}
-	defer db.Close()
-	//Установка базы
-	// insert, err :=db.Query("INSERT INTO `user` (`name`,`age`) VALUES ('Evg',34)")
-	// if err!=nil {
-	// 	panic(err)
-	// }
-	// defer insert.Close()
-	//handleRequest()
-	//Выборка 
-	res, err :=db.Query("SELECT `name`, `age` FROM `user`")
-	if err != nil {
-		
-	}
-	for res.Next(){
-		var user User
-		err = res.Scan(&user.Name, &user.Age)
-		if err != nil {
-		
-		}
-		fmt.Println(fmt.Sprintf("User: %s with age %d", user.Name, user.Age))
-	}
-	
-
+func main(){
+	handleFunc()
 }
